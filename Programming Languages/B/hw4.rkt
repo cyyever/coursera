c05611df48e9a4c208baed5ccb4199df1154565d
cyyever <cyyever@outlook.com>
2016-10-13 22:07:51 +0800

    完成作业

--- Programming Languages/B/hw4.rkt
+++ Programming Languages/B/hw4.rkt
@@ -1,4 +1,3 @@
-
 #lang racket
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
@@ -20,10 +19,10 @@
   (if (< n 0)
       (error "list-nth-mod: negative number")
       (if (null? xs)
-           (error "list-nth-mod: empty list")
-           (car (list-tail xs (remainder n (length xs))))
+          (error "list-nth-mod: empty list")
+          (car (list-tail xs (remainder n (length xs))))
+          )
       )
-  )
   )
 
 (define (stream-for-n-steps s n)
@@ -32,16 +31,16 @@
       (let ([pair (s)])
         (cons (car pair) (stream-for-n-steps (cdr pair) (- n 1))) 
         )
-  )
+      )
   )
 
 (define (funny-number-stream)
   (letrec
-    ([f (lambda (n)
-          (cons
-           ((if (= 0 (remainder n 5)) - +) 0 n)
-           (lambda () (f (+ n 1))))
-        )])
+      ([f (lambda (n)
+            (cons
+             ((if (= 0 (remainder n 5)) - +) 0 n)
+             (lambda () (f (+ n 1))))
+            )])
     (f 1)
     )
   )
@@ -55,14 +54,14 @@
     )
   (f 1)
   )
- 
+
 (define (stream-add-zero s)
   (lambda ()
     (let ([pair (s)])
       (cons
        (cons 0 (car pair))
        (stream-add-zero (cdr pair)))
-     )
+      )
     )
   )
 
@@ -75,8 +74,8 @@
        )
       )
     (f 0)
+    )
   )
-)
 
 (define (vector-assoc v vec)
   (define (find n)
@@ -85,4 +84,37 @@
         (let ([elm (vector-ref vec n)])
           (if (and (pair? elm) (equal? (car elm) v)) elm (find (+ n 1))))))
   (find 0)   
-)
+  )
+
+(define (cached-assoc xs n)
+  (letrec ([cache (make-vector n #f)]
+           [next-slot 0]
+           )
+    (lambda (v)
+      (let ([cached-res (vector-assoc v cache)])
+      (if cached-res
+          cached-res
+          (let ([res (assoc v xs)])
+            (if res
+                (begin
+                  (vector-set! cache next-slot res)
+                  (set! next-slot (remainder (+ next-slot 1) n))
+                  res
+                 )
+                res)
+            )
+          )
+        )
+      )
+    )
+  )
+    
+(define-syntax while-less
+      (syntax-rules (do)
+        [(while-less e1 do e2)
+         (letrec
+             ([res1 e1]
+              [do_once (lambda () (if (>= e2 res1) #t (do_once)))])
+           (do_once))]
+        )
+      )
