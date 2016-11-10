ee0772402d22b78a61a6b87e7b0868c86846a275
cyyever <cyyever@outlook.com>
2016-11-10 08:30:11 +0800

    新的额外练习

---
+++ Programming Languages/C/section-8-solution.rb
@@ -0,0 +1,193 @@
+require_relative './section-8-provided'
+
+class Character
+  def initialize hp
+    @hp = hp
+  end
+
+  def resolve_encounter enc
+    if !is_dead?
+      play_out_encounter enc
+    end
+  end
+
+  def is_dead?
+    @hp <= 0
+  end
+
+  private
+
+  def play_out_encounter enc
+    ## YOUR CODE HERE
+    enc.hit_character self
+  end
+end
+
+class Knight < Character
+  def initialize(hp, ap)
+    super hp
+    @ap = ap
+  end
+
+  def to_s
+    "HP: " + @hp.to_s + " AP: " + @ap.to_s
+  end
+
+  ## YOUR CODE HERE
+
+  def play_out_floor_trap trap
+    damage_me(trap.dam)
+  end
+
+  def play_out_monster monster
+    damage_me(monster.dam)
+  end
+
+  def play_out_potion potion
+    @hp += potion.hp
+  end
+
+  def play_out_armor armor
+    @ap += armor.ap
+  end
+  
+  private
+  def damage_me dam
+    if @ap == 0
+    then
+      @hp -= dam
+    elsif dam > @ap
+    then
+      damage_me(dam - @ap)
+      @ap = 0
+    else
+      @ap -= dam
+    end
+  end
+end
+
+class Wizard < Character
+  def initialize(hp, mp)
+    super hp
+    @mp = mp
+  end
+
+  def to_s
+    "HP: " + @hp.to_s + " MP: " + @mp.to_s
+  end
+
+  ## YOUR CODE HER
+  def is_dead?
+   super || @mp <= 0
+  end
+
+  def play_out_floor_trap trap
+    if @mp > 0
+    then
+      @mp -= 1
+    else
+      @hp -= trap.dam
+    end
+  end
+    
+  def play_out_monster monster
+    @mp -= monster.dam
+  end
+
+  def play_out_potion potion
+    @hp += potion.hp
+    @mp += potion.mp
+  end
+
+  def play_out_armor armor
+  end
+end
+
+class FloorTrap < Encounter
+  attr_reader :dam
+
+  def initialize dam
+    @dam = dam
+  end
+
+  def to_s
+    "A deadly floor trap dealing " + @dam.to_s + " point(s) of damage lies ahead!"
+  end
+
+  ## YOUR CODE HERE
+  def hit_character cht
+    cht.play_out_floor_trap self
+  end
+end
+
+class Monster < Encounter
+  attr_reader :dam, :hp
+
+  def initialize(dam, hp)
+    @dam = dam
+    @hp = hp
+  end
+
+  def to_s
+    "A horrible monster lurks in the shadows ahead. It can attack for " +
+        @dam.to_s + " point(s) of damage and has " +
+        @hp.to_s + " hitpoint(s)."
+  end
+
+  ## YOUR CODE HERE
+  
+  def hit_character cht
+    cht.play_out_monster self
+  end
+end
+
+class Potion < Encounter
+  attr_reader :hp, :mp
+
+  def initialize(hp, mp)
+    @hp = hp
+    @mp = mp
+  end
+
+  def to_s
+    "There is a potion here that can restore " + @hp.to_s +
+        " hitpoint(s) and " + @mp.to_s + " mana point(s)."
+  end
+
+  ## YOUR CODE HERE
+
+  def hit_character cht
+    cht.play_out_potion self
+  end
+end
+
+class Armor < Encounter
+  attr_reader :ap
+
+  def initialize ap
+    @ap = ap
+  end
+
+  def to_s
+    "A shiny piece of armor, rated for " + @ap.to_s +
+        " AP, is gathering dust in an alcove!"
+  end
+
+  ## YOUR CODE HERE
+
+  def hit_character cht
+    cht.play_out_armor self
+  end
+end
+
+if __FILE__ == $0
+  Adventure.new(Stdout.new, Knight.new(15, 3),
+    [Monster.new(1, 1),
+    FloorTrap.new(3),
+    Monster.new(5, 3),
+    Potion.new(5, 5),
+    Monster.new(1, 15),
+    Armor.new(10),
+    FloorTrap.new(5),
+    Monster.new(10, 10)]).play_out
+end
